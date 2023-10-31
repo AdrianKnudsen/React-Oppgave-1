ad39c8930b59aeefb926eda384901260ce351553
AdrianK-B06 <adriank.b06.kodehode@gmail.com>
2023-10-31 18:49:46 +0100

    first content commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+  <div>
+
+    <header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
+    <div className="search-bar">
+        <input type="text" placeholder="Search"></input>
+        <button className="search-btn">Search</button>
+    </div>
+    </header>
+
+<main>
+
+    <h1>Tech Blog</h1>
+
+    <section className="blog-post">
+          <h2>The Future of Artificial Intelligence</h2>
+          <p>Artificial Intelligence (AI) is advancing at an astonishing pace. 
+            From self-driving cars to natural language processing, this article explores the latest breakthroughs in AI and their potential impact on various industries.</p>
+            <p className="read-more">Read More...</p>
+        <button className="like-button">Like</button>
+    </section>
+
+    <section className="blog-post">
+          <h2>Blockchain Technology: Beyond Cryptocurrency</h2>
+          <p>Blockchain, the technology behind cryptocurrencies like Bitcoin, has applications far beyond digital currencies. Learn how blockchain is revolutionizing supply chain management and data security.</p>
+          <p className="read-more">Read More...</p>
+        <button className="like-button">Like</button>
+    </section>
+
+    <section className="blog-post">
+          <h2>The Rise of Quantum Computing</h2>
+          <p>Quantum computing is poised to transform computing as we know it. Explore the fundamental concepts of quantum computing and the potential for solving complex problems that are currently beyond classical computers.</p>
+          <p className="read-more">Read More...</p>
+        <button className="like-button">Like</button>
+    </section>
+</main>
+
+<footer>
+    
+      <p>© Kodehode 2023</p>
+    
+</footer>
+
+</div>
+
   )
 }
 
