fe0798169d8dc26d186b32db136477e91341ce8e
AdrianK-B06 <adriank.b06.kodehode@gmail.com>
2023-11-02 22:21:21 +0100

    added a new component
    added new component that receives and uses properties, and made a edit on a component so that it uses children properties to generate a read more function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,53 @@
 import "./App.css";
 import NavBar from "./components/navBar/navBar.tsx";
+import BlogPost from "./components/blogpost/BlogPost.tsx";
 
 function App() {
+  const blogPosts = [
+    {
+      id: 1,
+      title: "The Future of Artificial Intelligence",
+      content:
+        "Artificial Intelligence (AI) is advancing at an astonishing pace. From self-driving cars to natural language processing, this article explores the latest breakthroughs in AI and their potential impact on various industries.",
+      readMore: "Read More...",
+      additionalContent:
+        "As AI continues to advance, it opens up new possibilities for automation and innovation. In this blog post, we delve deeper into the ethical considerations surrounding AI and its implications for job markets.",
+    },
+    {
+      id: 2,
+      title: "Blockchain Technology: Beyond Cryptocurrency",
+      content:
+        "Blockchain, the technology behind cryptocurrencies like Bitcoin, has applications far beyond digital currencies. Learn how blockchain is revolutionizing supply chain management and data security.",
+      readMore: "Read More...",
+      additionalContent:
+        "Blockchain's decentralized ledger system has the potential to enhance data security and integrity in various industries. In this blog post, we explore real-world use cases of blockchain technology beyond cryptocurrencies.",
+    },
+    {
+      id: 3,
+      title: "The Rise of Quantum Computing",
+      content:
+        "Quantum computing is poised to transform computing as we know it. Explore the fundamental concepts of quantum computing and the potential for solving complex problems that are currently beyond classical computers.",
+      readMore: "Read More...",
+      additionalContent:
+        "Quantum computing's promise lies in its ability to tackle complex problems in fields such as cryptography and drug discovery. In this blog post, we discuss the race to develop practical quantum computers and their potential impact.",
+    },
+  ];
+
   return (
     <div>
       <NavBar />
       <main>
         <h1>Tech Blog</h1>
 
-        <section className="blog-post">
-          <h2>The Future of Artificial Intelligence</h2>
-          <p>
-            Artificial Intelligence (AI) is advancing at an astonishing pace.
-            From self-driving cars to natural language processing, this article
-            explores the latest breakthroughs in AI and their potential impact
-            on various industries.
-          </p>
-          <p className="read-more">Read More...</p>
-          <button className="like-button">Like</button>
-        </section>
-
-        <section className="blog-post">
-          <h2>Blockchain Technology: Beyond Cryptocurrency</h2>
-          <p>
-            Blockchain, the technology behind cryptocurrencies like Bitcoin, has
-            applications far beyond digital currencies. Learn how blockchain is
-            revolutionizing supply chain management and data security.
-          </p>
-          <p className="read-more">Read More...</p>
-          <button className="like-button">Like</button>
-        </section>
-
-        <section className="blog-post">
-          <h2>The Rise of Quantum Computing</h2>
-          <p>
-            Quantum computing is poised to transform computing as we know it.
-            Explore the fundamental concepts of quantum computing and the
-            potential for solving complex problems that are currently beyond
-            classical computers.
-          </p>
-          <p className="read-more">Read More...</p>
-          <button className="like-button">Like</button>
-        </section>
+        {blogPosts.map((post) => (
+          <BlogPost
+            key={post.id}
+            title={post.title}
+            initialContent={post.content}
+            additionalContent={post.additionalContent}
+            readMore="Read More..."
+          />
+        ))}
       </main>
 
       <footer>
---
+++ src/components/blogpost/BlogPost.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+console.log(React);
+
+type BlogPostProps = {
+  title: string;
+  initialContent: string;
+  additionalContent: string;
+  readMore: string;
+};
+
+export default function BlogPost(props: BlogPostProps) {
+  const [showAdditionalContent, setShowAdditionalContent] = useState(false);
+
+  const toggleShowAdditionalContent = () => {
+    setShowAdditionalContent(!showAdditionalContent);
+  };
+
+  const displayContent = showAdditionalContent
+    ? props.initialContent + " " + props.additionalContent
+    : props.initialContent;
+
+  return (
+    <div>
+      <section className="blog-post">
+        <h2>{props.title}</h2>
+        <p>{displayContent}</p>
+        <p className="read-more" onClick={toggleShowAdditionalContent}>
+          {showAdditionalContent ? "Read Less" : props.readMore}
+        </p>
+        <button className="like-button">Like</button>
+      </section>
+    </div>
+  );
+}
