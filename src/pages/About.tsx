3d94629de7fb87af435ec71f14f1f683e4d7613e
AdrianK-B06 <adriank.b06.kodehode@gmail.com>
2023-11-15 14:44:11 +0100

    added text to the about page and styling

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,8 +5,33 @@ const About: React.FC = () => {
   return (
     <div>
       <NavBar>{}</NavBar>
-
-      <h1>About Page</h1>
+      <div className="about-container">
+        <h1>About Us</h1>
+        <p>
+          Welcome to our technology hub! We are a diverse team of experts,
+          enthusiasts, and creators with a shared passion for exploring the
+          possibilities of the digital world.
+        </p>
+        <p>
+          Our journey began with a simple idea: to create a space where people
+          can stay informed, inspired, and connected with the latest trends in
+          technology. Whether you're a seasoned professional or just starting to
+          dip your toes into the vast ocean of tech, we've got something for
+          everyone.
+        </p>
+        <h2>What We Offer:</h2>
+        <ul>
+          <li>Insightful articles on cutting-edge technologies</li>
+          <li>Tutorials and guides for tech enthusiasts</li>
+          <li>Product reviews and recommendations</li>
+          <li>Community forums for discussions and Q&A</li>
+        </ul>
+        <p>
+          Join us on this exciting adventure, and let's navigate the digital
+          landscape together. Feel free to reach out, engage in conversations,
+          and become a part of our growing community!
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,6 @@ const NotFound: React.FC = () => {
   return (
     <div>
       <h1>404 - Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
     </div>
   );
 };
